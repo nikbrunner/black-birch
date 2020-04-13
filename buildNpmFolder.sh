1be5fd2b0ba024e96f9c0de8419bf48df97efd23
Nikolaus Brunner <nikolaus.brunner@diva-e.com>
2020-04-13 15:53:47 +0200

    Update for all files. Fixed some bugs and brocken imports

--- buildNpmFolder.sh
+++ buildNpmFolder.sh
@@ -3,6 +3,6 @@
 yes | cp ./stillness/dsi/_uni-space.scss ./stillness/npm/_uni-space.scss
 yes | cp ./stillness/dsi/_mixins.scss ./stillness/npm/_mixins.scss
 yes | cp ./stillness/dsi/_keyframes.scss ./stillness/npm/_keyframes.scss
-yes | cp ./stillness/dsi/_keyframes.scss ./stillness/npm/_responsive-design.scss
+yes | cp ./stillness/dsi/_responsive-design.scss ./stillness/npm/_responsive-design.scss
 
 echo 'Copied all neccessery partials to NPM folder!'
